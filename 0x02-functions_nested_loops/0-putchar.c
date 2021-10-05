732806111d11b30dd765b3afc781294a62ccb1ca
EricAltez <ericaltez1111@gmail.com>
2021-10-05 16:32:40 -0700

    Write a program that prints _putchar, followed by a new line using local function _putchar.

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,23 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
-* main - Entry point
+* main - writes the character c to stdout
 *
-* Return: Always 0 (Success)
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
 */
+
 int main(void)
 {
-	printf("_putchar");
-	putchar('\n');
-
+	_putchar(95);
+	_putchar(112);
+	_putchar(117);
+	_putchar(116);
+	_putchar(99);
+	_putchar(104);
+	_putchar(97);
+	_putchar(114);
+	_putchar('\n');
 	return (0);
+
 }
