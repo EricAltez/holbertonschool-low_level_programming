d385356f3a8a72b695df94448d5df5cd92db5b96
EricAltez <ericaltez1111@gmail.com>
2021-10-12 09:01:30 -0700

    adas

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,26 +12,13 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b)
-	{
-		if (a > c)
-		{
-			largest = a;
-		}
-	}
-	if (b > a)
-	{
-		if (b > c)
-		{
-			largest = b;
-		}
-	}
-	if (c > a)
-	{
-		if (c > b)
-		{
-			largest = c;
-		}
-	}
+	if (a >= b && a >= c)
+		largest = a;
+
+	if (b >= a && b >= c)
+		largest = b;
+
+	if (c >= a && c >= b)
+		largest = c;
 	return (largest);
 }
