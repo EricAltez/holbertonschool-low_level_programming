2d1dc28b8b080c13a7c02c7a8287d34ff0de7c3e
EricAltez <ericaltez1111@gmail.com>
2021-10-21 05:53:57 -0700

    Write a function that returns the value of x raised to the power of y.

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *_sqrt_recursion - 
+ *@n: 
+ *Return: 
+ */
+int _sqrt_recursion(int n)
+{
+
+
+
+
+return();
+}
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *is_prime_number
+ *@n:
+ *Return:
+ */
+int is_prime_number(int n)
+{
+
+
+
+
+return();
+}
