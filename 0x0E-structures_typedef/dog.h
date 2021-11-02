404ba8c625bb0f47f55ea2c0cb0eeb5b77f6bce2
EricAltez <ericaltez1111@gmail.com>
2021-11-02 05:17:50 -0700

    add rearme file and first ex file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *struct dog - dog data
+ *@name: dogs name
+ *@age: dogs age
+ *@owner: dogs owner
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
