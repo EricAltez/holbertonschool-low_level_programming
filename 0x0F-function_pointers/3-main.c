070d8589e3826686d487a7b8c43118b0879fd20c
EricAltez <ericaltez1111@gmail.com>
2021-11-13 13:25:22 -0800

    Write a program that performs simple operations.

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -12,28 +12,30 @@ int main(int argc, char *argv[])
 	int result = 0;
 	int arg1 = 0;
 	int arg2 = 0;
-	char *operator = NULL;
+	char *o;
+	int (*func)(int, int);
 
 	arg1 = atoi(argv[1]);
 	arg2 = atoi(argv[3]);
-	operator = argv[2];
+	o = argv[2];
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 			exit(98);
 	}
-	result = get_op_func(operator)(arg1, arg2);
-	if (result == '0')
+	func = get_op_func(argv[2]);
+	if (!func)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if ((*operator == '/' || *operator == '%') && (argv[3] == 0))
+	if ((*o == '/' || *o == '%') && (argv[3] == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
+	result = func(arg1, arg2);
 	printf("%i\n", result);
 	return (0);
 }
