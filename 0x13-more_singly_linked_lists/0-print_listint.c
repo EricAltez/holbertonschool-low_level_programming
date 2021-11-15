7b1ff1683a07d43cb3c7da8eebacbcd939072af9
EricAltez <ericaltez1111@gmail.com>
2021-11-15 10:59:05 -0800

    add header file and started ex 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *print_listint - prints all the elements of a listint_t list
+ *@h: 
+ *Return: number of elements
+ */
+size_t print_listint(const listint_t *h)
+{
+printf("%d", head);
+return();
+}
