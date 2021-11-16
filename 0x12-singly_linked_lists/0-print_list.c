e062e5e7cec8c70ce602fdb820f4066511aecea9
EricAltez <ericaltez1111@gmail.com>
2021-11-16 13:01:27 -0800

    corrected errors

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,20 +12,16 @@ size_t print_list(const list_t *h)
 	/**node counter*/
 	size_t c = 0;
 
-if (h == NULL)
-{
-	return (0);
-}
-	while (h != NULL)
+	while (h)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
 		{
-			printf("[%d] %s\n", h->len, h->str);
+			printf("[%u] %s\n", h->len, h->str);
 		}
-		c++;
 		h = h->next;
+		c++;
 	}
 	return (c);
 }
