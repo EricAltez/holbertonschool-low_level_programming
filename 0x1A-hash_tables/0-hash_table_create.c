69a396046445a0581407a20aed919e7d13a9dc7b
EricAltez <ericaltez1111@gmail.com>
2022-01-28 12:21:39 -0800

    add readme, header and 0 file

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ *hash_table_create - creates hash table
+ *@size: size of the array
+ *Return: pointer to the newly created hash table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_h_table = NULL;
+	unsigned long int i = 0;
+
+	new_h_table = malloc(sizeof(hash_table_t));
+	if (!new_h_table)
+		return (NULL);
+
+	new_h_table->size = size;
+	new_h_table->array = malloc(size * sizeof(hash_node_t));
+	if (!new_h_table->array)
+	{
+		free(new_h_table);
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+		new_h_table->array[i] = NULL;
+
+return (new_h_table);
+}
