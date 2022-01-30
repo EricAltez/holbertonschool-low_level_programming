1745371f5ba7406adb39b7823988054223ff0809
EricAltez <ericaltez1111@gmail.com>
2022-01-30 15:42:03 -0800

    add file 5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ *hash_table_print - prints a hash table
+ *@ht: hash table to be printed
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i = 0;
+	hash_node_t *aux;
+	char *sep = "";
+
+	if (!ht)
+		return;
+
+	printf("{");
+
+	while (i < ht->size)
+	{
+		aux = ht->array[i];
+		while (aux)
+		{
+			printf("%s'%s' :'%s'", sep, aux->key, aux->value);
+			sep = ", ";
+			aux = aux->next;
+		}
+		i++;
+	}
+	printf("}\n");
+}
