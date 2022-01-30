223527b15b501149eaa8a0d790ad3806d0289f4d
EricAltez <ericaltez1111@gmail.com>
2022-01-30 11:47:44 -0800

    corrected file 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,22 +9,31 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int kidx = 0, size = 0;
+	unsigned long int kidx = 0;
 	hash_node_t *new_node;
+
+	unsigned const char *dkey = (unsigned const char *)key;
 
 	if (!ht || !key || !value)
 		return (0);
 
-	kidx = key_index((unsigned char *)key, size);
+	kidx = key_index(dkey, ht->size);
 	new_node = malloc(sizeof(hash_node_t));
 	if (!new_node)
 		return (0);
+
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
+	new_node->next = NULL;
+
+	if (ht->array[kidx] == NULL)
+	{
+		ht->array[kidx] = new_node;
+		return (1);
+	}
+
 	new_node->next = ht->array[kidx];
-
-
-
+	ht->array[kidx] = new_node;
 
 return (1);
 }
