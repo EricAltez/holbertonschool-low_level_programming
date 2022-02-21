542312392e9f86b868524477f4c31f62bbfc1459
EricAltez <ericaltez1111@gmail.com>
2022-02-21 12:11:23 -0800

    add old file

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *_strstr - asdasd
+ *@haystack: asd
+ *@needle: asd
+ *Return: asd
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0, j = 0, k = 0;
+
+	while (haystack[i] != 0)
+	{
+		k = i;
+		j = 0;
+		if (haystack[k] == needle[j])
+		{
+			while (needle[j] == haystack[k])
+			{
+				j++;
+				k++;
+			}
+			if (needle[j] == '\0')
+				return (haystack + i);
+		}
+		i++;
+	}
+return (NULL);
+}
